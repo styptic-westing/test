511cc162f1b312a760090804a1658288aed315dc
IanNosov <g2.1.0@icloud.com>
2024-07-26 01:23:41 +0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Yan on 26/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
